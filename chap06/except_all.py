dfbaa8842282090a7b8cf4353e5969b896af59d4
SurinJeon <surin0901@naver.com>
2021-05-31 10:19:16 +0900

    study chap07 exception handling

---
+++ chap06/except_all.py
@@ -0,0 +1,18 @@
+#변수 선언
+list_number = [52, 273, 32, 72, 100]
+
+# try-except 구문으로 예외 처리
+try:
+    number_input = int(input("정수 입력 > "))
+
+    print("{}번째 요소: {}".format(number_input, list_number[number_input]))
+    예외.발생해주세요()
+except ValueError as exception:
+    print("정수를 입력해주세요.")
+    print(type(exception), exception)
+except IndexError as exception:
+    print("리스트의 인덱스를 벗어났습니다.")
+    print(type(exception), exception)
+except Exception as exception:
+    print("미리 파악하지 못한 예외가 발생했습니다.")
+    print(type(exception), exception)
