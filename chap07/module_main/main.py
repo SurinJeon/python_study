485755d4fc5bd121cda92e17252e33d7640e30e0
SurinJeon <surin0901@naver.com>
2021-06-02 09:47:36 +0900

    study chap07(module) customize module

---
+++ chap07/module_main/main.py
@@ -0,0 +1,7 @@
+import test_module as teest
+
+
+if __name__ == "__main__":
+    print("#메인의 __name__ 출력하기")
+    print(__name__)
+    print()
---
+++ chap07/module_main/test_module.py
@@ -0,0 +1,4 @@
+if __name__ == "test_module":
+    print("#모듈의 __name__ 출력하기")
+    print(__name__)
+    print()
